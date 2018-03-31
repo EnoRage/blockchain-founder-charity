dbeb0359bf2cab0a3feced40035a851ea005e50d
Кирилл <krboktv@gmail.com>
2018-03-31 12:51:48 +0300

    Распарсил курсы

--- bot.go
+++ bot.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"time"
 
 	"./course"
+
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
+var courseJSON []byte
+
+var courseInterface interface{}
+
 func main() {
 	b, err := tb.NewBot(tb.Settings{
-		Token:  "576497547:AAFqeiPb5j5fVktRPqtzpTvaIp8ExKlZZAY",
+		Token: "576497547:AAFqeiPb5j5fVktRPqtzpTvaIp8ExKlZZAY",
+
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
 	})
 
-	course.Course("USD")
+	courseJSON = course.Course("USD")
+	// fmt.Printf("%v\n", p)
+	json.Unmarshal(courseJSON, &courseInterface)
+	fmt.Printf("%+v\n", courseInterface)
 
 	if err != nil {
 		log.Fatal(err)
