88c795931afb56a8a49ee237c360756ca2d6e8e7
Кирилл <krboktv@gmail.com>
2018-04-01 01:56:41 +0300

    описание объектов валют

--- objects/objects.go
+++ objects/objects.go
@@ -6,12 +6,25 @@ type currency struct {
 	assetID string
 }
 
-var Baves = currency{name: "Waves", ticker: "WAVES", assetID: "WAVES"}
+// Waves Объект криптовалюты Waves
+var Waves = currency{name: "Waves", ticker: "WAVES", assetID: "WAVES"}
+
+// Bitcoin Объект токена Bitcoin
 var Bitcoin = currency{name: "Bitcoin", ticker: "BTC", assetID: "8LQW8f7P5d5PZM7GtZEBgaqRPGSzS3DfPuiXrURJ4AJS"}
+
+// Ethereum Объект токена Ethereum
 var Ethereum = currency{name: "Ethereum", ticker: "ETH", assetID: "474jTeYx2r2Va35794tCScAXWJG9hU2HcgxzMowaZUnu"}
+
+// Litecoin Объект токена Litecoin
 var Litecoin = currency{name: "Litecoin", ticker: "LTC", assetID: "HZk1mbfuJpmxU1Fs4AX5MWLVYtctsNcg6e2C6VKqK8zk"}
+
+// ZCash Объект токена ZCash
 var ZCash = currency{name: "ZCash", ticker: "ZEC", assetID: "BrjUWjndUanm5VsJkbUip8VRYy6LWJePtxya3FNv4TQa"}
+
+// UsDollar Объект токена USD
 var UsDollar = currency{name: "US Dollar", ticker: "USD", assetID: "Ft8X1v1LTa1ABafufpaCWyVj8KkaxUWE6xBhW6sNFJck"}
+
+// Euro Объект токена Euro
 var Euro = currency{name: "Euro", ticker: "EUR", assetID: "Gtb1WRznfchDnTh37ezoDTJ4wcoKaRsKqKjJjy7nm2zU"}
 
 // GetName Получение названия валюты
