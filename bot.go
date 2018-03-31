21850781e59ee1bea19a4601b3ae96833139724f
Your Name <you@example.com>
2018-03-31 13:58:13 +0300

    fond

--- bot.go
+++ bot.go
@@ -11,6 +11,11 @@ import (
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
+// для диалогов переменные
+var fond = ""
+
+// для диалогов переменные
+
 var courseJSON []byte
 var courseArray gjson.Result
 var courseInterface interface{}
@@ -33,18 +38,23 @@ func main() {
 	replyKeys := [][]tb.ReplyButton{
 		{replyBtn1, replyBtn2},
 	}
-	inlineBtn1 := tb.InlineButton{Unique: "0", Text: "0️⃣"}
-	inlineBtn2 := tb.InlineButton{Unique: "1", Text: "1️⃣"}
-	inlineBtn3 := tb.InlineButton{Unique: "2", Text: "2️⃣"}
-	inlineBtn4 := tb.InlineButton{Unique: "3", Text: "3️⃣"}
-	inlineBtn5 := tb.InlineButton{Unique: "4", Text: "4️⃣"}
-	inlineBtn6 := tb.InlineButton{Unique: "5", Text: "5️⃣"}
-	inlineBtn7 := tb.InlineButton{Unique: "6", Text: "6️⃣"}
-	inlineBtn8 := tb.InlineButton{Unique: "7", Text: "7️⃣"}
-	inlineBtn9 := tb.InlineButton{Unique: "8", Text: "8️⃣"}
+	inlineBtn1 := tb.InlineButton{Unique: "1", Text: "1️⃣"}
+	inlineBtn2 := tb.InlineButton{Unique: "2", Text: "2️⃣"}
+	inlineBtn3 := tb.InlineButton{Unique: "3", Text: "3️⃣"}
+	inlineBtn4 := tb.InlineButton{Unique: "4", Text: "4️⃣"}
+	inlineBtn5 := tb.InlineButton{Unique: "5", Text: "5️⃣"}
+	inlineBtn6 := tb.InlineButton{Unique: "6", Text: "6️⃣"}
+	inlineBtn7 := tb.InlineButton{Unique: "7", Text: "7️⃣"}
+	inlineBtn8 := tb.InlineButton{Unique: "8", Text: "8️⃣"}
+	inlineBtn9 := tb.InlineButton{Unique: "9", Text: "9️⃣"}
 
 	inlineKbrdCalc := [][]tb.InlineButton{
 		{inlineBtn1, inlineBtn2, inlineBtn3, inlineBtn4, inlineBtn5, inlineBtn6, inlineBtn7, inlineBtn8, inlineBtn9},
+	}
+
+	inlineInv := tb.InlineButton{Unique: "inv", Text: "Перевести"}
+	inlineInvMenu := [][]tb.InlineButton{
+		{inlineInv},
 	}
 	course.Course("USD")
 
@@ -59,14 +69,15 @@ func main() {
 		}
 		b.Send(m.Sender, "Главное меню", &tb.ReplyMarkup{ReplyKeyboard: replyKeys})
 	})
-
+	// тут переход в список фондов с пожертвованиями
 	b.Handle(&replyBtn2, func(m *tb.Message) {
 		b.Send(m.Sender, orglist.Data, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
 	})
+	// тут переход в список фондов с пожертвованиями
 
-	// inline buttons 1-9
+	// inline buttons 1-9 Инфа о фондах
 	b.Handle(&inlineBtn1, func(c *tb.Callback) {
-		b.Edit(c.Message, orglist.Data2, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
+		b.Edit(c.Message, orglist.Data1, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
 		b.Respond(c, &tb.CallbackResponse{})
 	})
 	b.Handle(&inlineBtn2, func(c *tb.Callback) {
@@ -74,33 +85,64 @@ func main() {
 		b.Respond(c, &tb.CallbackResponse{})
 	})
 	b.Handle(&inlineBtn3, func(c *tb.Callback) {
-		b.Edit(c.Message, orglist.Data2, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
+		b.Edit(c.Message, orglist.Data3, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
 		b.Respond(c, &tb.CallbackResponse{})
 	})
 	b.Handle(&inlineBtn4, func(c *tb.Callback) {
-		b.Edit(c.Message, orglist.Data2, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
+		b.Edit(c.Message, orglist.Data4, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
 		b.Respond(c, &tb.CallbackResponse{})
 	})
 	b.Handle(&inlineBtn5, func(c *tb.Callback) {
-		b.Edit(c.Message, orglist.Data2, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
+		b.Edit(c.Message, orglist.Data5, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
 		b.Respond(c, &tb.CallbackResponse{})
 	})
 	b.Handle(&inlineBtn6, func(c *tb.Callback) {
-		b.Edit(c.Message, orglist.Data2, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
+		b.Edit(c.Message, orglist.Data6, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
 		b.Respond(c, &tb.CallbackResponse{})
 	})
 	b.Handle(&inlineBtn7, func(c *tb.Callback) {
-		b.Edit(c.Message, orglist.Data2, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
+		b.Edit(c.Message, orglist.Data7, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
 		b.Respond(c, &tb.CallbackResponse{})
 	})
 	b.Handle(&inlineBtn8, func(c *tb.Callback) {
-		b.Edit(c.Message, orglist.Data2, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
+		b.Edit(c.Message, orglist.Data8, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
 		b.Respond(c, &tb.CallbackResponse{})
 	})
 	b.Handle(&inlineBtn9, func(c *tb.Callback) {
-		b.Edit(c.Message, orglist.Data2, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
+		b.Edit(c.Message, orglist.Data9, &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineKbrdCalc})
 		b.Respond(c, &tb.CallbackResponse{})
 	})
 	// inline buttons 1-9
+
+	// слушает какой фонд выбрал
+	b.Handle("/fond0", func(m *tb.Message) {
+		b.Send(m.Sender, "0 фонд тут кнопочки купить и подробнее инфа", &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineInvMenu})
+	})
+	b.Handle("/fond1", func(m *tb.Message) {
+		b.Send(m.Sender, "Первый фонд тут кнопочки купить и подробнее инфа", &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineInvMenu})
+	})
+	b.Handle("/fond2", func(m *tb.Message) {
+		b.Send(m.Sender, "2 фонд тут кнопочки купить и подробнее инфа", &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineInvMenu})
+	})
+	b.Handle("/fond3", func(m *tb.Message) {
+		b.Send(m.Sender, "3 фонд тут кнопочки купить и подробнее инфа", &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineInvMenu})
+	})
+	b.Handle("/fond4", func(m *tb.Message) {
+		b.Send(m.Sender, "4 фонд тут кнопочки купить и подробнее инфа", &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineInvMenu})
+	})
+	b.Handle("/fond5", func(m *tb.Message) {
+		b.Send(m.Sender, "5 фонд тут кнопочки купить и подробнее инфа", &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineInvMenu})
+	})
+	b.Handle("/fond6", func(m *tb.Message) {
+		b.Send(m.Sender, "6 фонд тут кнопочки купить и подробнее инфа", &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineInvMenu})
+	})
+	b.Handle("/fond7", func(m *tb.Message) {
+		b.Send(m.Sender, "7 фонд тут кнопочки купить и подробнее инфа", &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineInvMenu})
+	})
+	b.Handle("/fond8", func(m *tb.Message) {
+		b.Send(m.Sender, "8 фонд тут кнопочки купить и подробнее инфа", &tb.SendOptions{ParseMode: "Markdown"}, &tb.ReplyMarkup{InlineKeyboard: inlineInvMenu})
+	})
+
+	// слушает какой фонд выбрал
 	b.Start()
 }
