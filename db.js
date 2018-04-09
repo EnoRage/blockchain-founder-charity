9d5fb4dde7eb660c7f99edf42ed10fd69e532cc6
Кирилл <krboktv@gmail.com>
2018-04-09 23:15:56 +0300

    fix

--- db.js
+++ db.js
@@ -3,15 +3,8 @@ const mongoose = require("mongoose"),
 
 mongoose.Promise = global.Promise;
 
-<<<<<<< HEAD
 const uri = 'mongodb://erage:doBH8993nnjdoBH8993nnj@51.144.89.99:27017/ImCup?authSource=admin'; // Мейн нет ПОМЕНЯТЬ
 
-=======
-// const user = 'admin';
-// const password = 'itss2018!';
-// const uri = 'mongodb://admin:itss2018\!@medicineassistantdb.westeurope.cloudapp.azure.com:27017/BlockChainDB?authSource=admin'; // Мейн нет ПОМЕНЯТЬ
-// const uri = 'mongodb://51.144.89.99:27017/Charity'; // Мейн нет ПОМЕНЯТЬ
->>>>>>> 090e816fd1f105d4fce912b40fbe98f29c3cd3d4
 const options = {
     autoIndex: false,
     reconnectTries: Number.MAX_VALUE,
